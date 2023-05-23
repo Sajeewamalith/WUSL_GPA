ff067a9e908c5a4ae6ed5aacccf1806436a865a7
Sajee <sajeewamalith0219@gmail.com>
2023-05-23 09:59:34 +0530

    add activities files

---
+++ app/src/main/java/com/example/wuslgpa/AddStudentsActivity.java
@@ -0,0 +1,14 @@
+package com.example.wuslgpa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddStudentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_students);
+    }
+}
