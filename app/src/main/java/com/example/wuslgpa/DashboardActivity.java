4f02f57b2cf4760e328e7569113a8805d892b852
Sajee <sajeewamalith0219@gmail.com>
2023-05-23 10:51:17 +0530

    Add floatingActionButton to add students

--- app/src/main/java/com/example/wuslgpa/DashboardActivity.java
+++ app/src/main/java/com/example/wuslgpa/DashboardActivity.java
@@ -2,7 +2,11 @@ package com.example.wuslgpa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        // Setup FAB to open AddStudentsActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DashboardActivity.this, AddStudentsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
