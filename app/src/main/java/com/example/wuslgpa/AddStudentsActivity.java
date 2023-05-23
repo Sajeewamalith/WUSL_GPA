b2a8e7f0461cda15a69f190ff5f9992b70c590e3
Sajee <sajeewamalith0219@gmail.com>
2023-05-23 15:48:41 +0530

    add app bar and save button to AddStudentsActivity

--- app/src/main/java/com/example/wuslgpa/AddStudentsActivity.java
+++ app/src/main/java/com/example/wuslgpa/AddStudentsActivity.java
@@ -1,8 +1,11 @@
 package com.example.wuslgpa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AddStudentsActivity extends AppCompatActivity {
 
@@ -10,5 +13,43 @@ public class AddStudentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_students);
+
+        // This is a new Student, so change the app bar to say "Add Students"
+        setTitle(getString(R.string.add_student_activity_title));
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_add_student, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                // Save Student to database
+                saveStudent();
+                // Exit activity
+                finish();
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    private void saveStudent() {
+
     }
 }
